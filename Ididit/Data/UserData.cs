6428ab55a0d80febb9f4cdddd7b0dd7e0c3ca53f
Urban <dzindzinovic@hotmail.com>
2024-01-10 19:28:05 +0100

    added UserData

---
+++ Ididit/Data/UserData.cs
@@ -0,0 +1,10 @@
+﻿using Ididit.Data.Entities;
+
+namespace Ididit.Data;
+
+public class UserData
+{
+    public List<HabitEntity>? Habits { get; set; }
+    public List<NoteEntity>? Notes { get; set; }
+    public List<TaskEntity>? Tasks { get; set; }
+}
