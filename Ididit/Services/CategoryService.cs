b3b8f4579f2423ce595088c3b1f2437d6e2183cb
Urban <dzindzinovic@hotmail.com>
2024-01-15 20:24:20 +0100

    update CategoryService

--- Ididit/Services/CategoryService.cs
+++ Ididit/Services/CategoryService.cs
@@ -1,8 +1,41 @@
 ﻿using Ididit.Data;
+using Ididit.Data.Entities;
+using Ididit.Data.Models;
 
 namespace Ididit.Services;
 
 public class CategoryService(IDataAccess dataAccess)
 {
     private readonly IDataAccess _dataAccess = dataAccess;
+
+    public Dictionary<long, CategoryModel>? Categories { get; set; }
+
+    public CategoryModel? SelectedCategory { get; set; }
+
+    public CategoryModel? NewCategory { get; set; }
+
+    public CategoryModel? EditCategory { get; set; }
+
+    public async Task Initialize()
+    {
+        if (Categories is null)
+        {
+            IReadOnlyList<CategoryEntity> categories = await _dataAccess.GetCategories();
+            Categories = categories.Select(c => new CategoryModel
+            {
+                Id = c.Id,
+                Title = c.Title,
+            }).ToDictionary(x => x.Id);
+        }
+
+        NewCategory ??= new();
+    }
+
+    public void SetSelectedCategory(long? id)
+    {
+        if (Categories is null)
+            return;
+
+        SelectedCategory = id.HasValue && Categories.TryGetValue(id.Value, out CategoryModel? category) ? category : null;
+    }
 }
