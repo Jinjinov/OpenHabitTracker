cdbdeb931bbefc7b0b143663dd71aeef85d2c924
Urban <dzindzinovic@hotmail.com>
2024-01-15 21:13:17 +0100

    updated CategoryService

--- Ididit/Services/CategoryService.cs
+++ Ididit/Services/CategoryService.cs
@@ -38,4 +38,48 @@ public class CategoryService(IDataAccess dataAccess)
 
         SelectedCategory = id.HasValue && Categories.TryGetValue(id.Value, out CategoryModel? category) ? category : null;
     }
+
+    public async Task AddCategory()
+    {
+        if (Categories is null || NewCategory is null)
+            return;
+
+        CategoryEntity category = new()
+        {
+            Title = NewCategory.Title
+        };
+
+        await _dataAccess.AddCategory(category);
+
+        NewCategory.Id = category.Id;
+
+        Categories.Add(NewCategory.Id, NewCategory);
+
+        NewCategory = null;
+    }
+
+    public async Task UpdateCategory()
+    {
+        if (Categories is null || EditCategory is null)
+            return;
+
+        if (await _dataAccess.GetCategory(EditCategory.Id) is CategoryEntity category)
+        {
+            category.Title = EditCategory.Title;
+
+            await _dataAccess.UpdateCategory(category);
+        }
+
+        EditCategory = null;
+    }
+
+    public async Task DeleteCategory(CategoryModel category)
+    {
+        if (Categories is null)
+            return;
+
+        Categories.Remove(category.Id);
+
+        await _dataAccess.RemoveCategory(category.Id);
+    }
 }
