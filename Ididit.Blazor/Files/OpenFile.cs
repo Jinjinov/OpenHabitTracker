e51b1bd262ad0ff5121bebe3bc0932c214cce346
Urban <dzindzinovic@hotmail.com>
2024-02-04 11:32:44 +0100

    added label to InputFile

--- Ididit.Blazor/Files/OpenFile.cs
+++ Ididit.Blazor/Files/OpenFile.cs
@@ -11,13 +11,20 @@ public class OpenFile : IOpenFile
     {
         return builder =>
         {
-            builder.OpenComponent(0, typeof(InputFile));
-            builder.AddAttribute(1, "OnChange", EventCallback.Factory.Create(this, async (InputFileChangeEventArgs args) =>
+            builder.OpenElement(0, "label");
+            builder.AddAttribute(1, "class", "btn btn-primary");
+            builder.AddContent(2, "Import");
+
+            builder.OpenComponent(3, typeof(InputFile));
+            builder.AddAttribute(4, "class", "d-none");
+            builder.AddAttribute(5, "OnChange", EventCallback.Factory.Create(this, async (InputFileChangeEventArgs args) =>
             {
                 Stream stream = args.File.OpenReadStream(maxAllowedSize: _maxAllowedFileSize);
                 await onFileOpened(args.File.Name, stream);
             }));
             builder.CloseComponent();
+
+            builder.CloseElement();
         };
     }
 }
