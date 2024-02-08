4e13122e26e6501b5787eabaf48cb35d8fab532c
Urban <dzindzinovic@hotmail.com>
2024-02-08 10:46:30 +0100

    added class CalendarParams

--- Ididit/Services/CalendarService.cs
+++ Ididit/Services/CalendarService.cs
@@ -1,11 +1,7 @@
-﻿using Ididit.Data;
+﻿namespace Ididit.Services;
 
-namespace Ididit.Services;
-
-public class CalendarService(AppData appData)
+public class CalendarService
 {
-    private readonly AppData _appData = appData;
-
     private readonly string[] _days = { "Su", "Mo", "Tu", "We", "Th", "Fr", "Sa" };
 
     private readonly Dictionary<long, DateTime> _dateByTicks = new();
@@ -13,32 +9,14 @@ public class CalendarService(AppData appData)
     private const long _ticksInDay = 864_000_000_000;
     private const long _ticksInWeek = 6_048_000_000_000;
 
-    private DateTime _calendarStart;
-
-    public DateTime FirstDayOfMonth { get; private set; }
-
-    public async Task Initialize()
+    public string GetDayOfWeek(DayOfWeek firstDayOfWeek, int dayIndex)
     {
-        await _appData.InitializeSettings();
-
-        SetCalendarStartByFirstDayOfMonth(DateTime.Today);
+        return _days[(dayIndex + (int)firstDayOfWeek) % 7];
     }
 
-    public string GetDayOfWeek(int dayIndex)
+    public DateTime GetCalendarDay(DateTime calendarStart, int calendarWeek, int dayInWeek)
     {
-        return _days[(dayIndex + (int)_appData.Settings.FirstDayOfWeek) % 7];
-    }
-
-    DateTime GetCalendarDay(int calendarWeek, int dayInWeek)
-    {
-        int days = calendarWeek * 7 + dayInWeek;
-
-        return _calendarStart.AddDays(days);
-    }
-
-    public DateTime GetCalendarDayWithCaching(int calendarWeek, int dayInWeek)
-    {
-        long ticks = _calendarStart.Ticks + calendarWeek * _ticksInWeek + dayInWeek * _ticksInDay;
+        long ticks = calendarStart.Ticks + calendarWeek * _ticksInWeek + dayInWeek * _ticksInDay;
 
         if (!_dateByTicks.TryGetValue(ticks, out DateTime date))
         {
@@ -47,56 +25,5 @@ public class CalendarService(AppData appData)
         }
 
         return date;
-    }
-
-    private DateTime GetFirstDayOfMonth(DateTime day)
-    {
-        return new DateTime(day.Year, day.Month, 1);
-    }
-
-    private DateTime GetFirstDateOfWeek(DateTime day)
-    {
-        int diff = -((7 + (day.DayOfWeek - _appData.Settings.FirstDayOfWeek)) % 7);
-        return day.AddDays(diff).Date;
-    }
-
-    void SetCalendarStartByFirstDayOfWeek(DateTime day)
-    {
-        FirstDayOfMonth = GetFirstDayOfMonth(day);
-        _calendarStart = GetFirstDateOfWeek(day);
-    }
-
-    void SetCalendarStartByFirstDayOfMonth(DateTime day)
-    {
-        FirstDayOfMonth = GetFirstDayOfMonth(day);
-        _calendarStart = GetFirstDateOfWeek(FirstDayOfMonth);
-    }
-
-    public void SetCalendarStartToNextMonth()
-    {
-        FirstDayOfMonth = FirstDayOfMonth.AddMonths(1);
-        _calendarStart = GetFirstDateOfWeek(FirstDayOfMonth);
-    }
-
-    public void SetCalendarStartToPreviousMonth()
-    {
-        FirstDayOfMonth = FirstDayOfMonth.AddMonths(-1);
-        _calendarStart = GetFirstDateOfWeek(FirstDayOfMonth);
-    }
-
-    public void SetCalendarStartToNextWeek()
-    {
-        _calendarStart = _calendarStart.AddDays(7);
-        FirstDayOfMonth = GetFirstDayOfMonth(_calendarStart);
-        if (_calendarStart.Day > 14)
-            FirstDayOfMonth = FirstDayOfMonth.AddMonths(1);
-    }
-
-    public void SetCalendarStartToPreviousWeek()
-    {
-        _calendarStart = _calendarStart.AddDays(-7);
-        FirstDayOfMonth = GetFirstDayOfMonth(_calendarStart);
-        if (_calendarStart.Day > 14)
-            FirstDayOfMonth = FirstDayOfMonth.AddMonths(1);
     }
 }
