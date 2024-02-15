d06cada4206d5a3ef7105c493ae1e06c68161ec3
Urban <dzindzinovic@hotmail.com>
2024-02-15 21:21:33 +0100

    added Note Task Habit component

--- Ididit.Blazor/wwwroot/jsInterop.js
+++ Ididit.Blazor/wwwroot/jsInterop.js
@@ -1,5 +1,5 @@
-// This is a JavaScript module that is loaded on demand. It can export any number of
-// functions, and may import other JavaScript modules if required.
+// This is a JavaScript module that is loaded on demand.
+// It can export any number of functions, and may import other JavaScript modules if required.
 
 export function showPrompt(message) {
   return prompt(message, 'Type anything here');
