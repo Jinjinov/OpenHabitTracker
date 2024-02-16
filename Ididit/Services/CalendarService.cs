ac6c8138ba96ab97af7ddd776666558d803249ba
Urban <dzindzinovic@hotmail.com>
2024-02-16 16:08:51 +0100

    added ColumnWidth

--- Ididit/Services/CalendarService.cs
+++ Ididit/Services/CalendarService.cs
@@ -7,16 +7,15 @@ public class CalendarService
     private readonly Dictionary<long, DateTime> _dateByTicks = new();
 
     private const long _ticksInDay = 864_000_000_000;
-    private const long _ticksInWeek = 6_048_000_000_000;
 
     public string GetDayOfWeek(DayOfWeek firstDayOfWeek, int dayIndex)
     {
         return _days[(dayIndex + (int)firstDayOfWeek) % 7];
     }
 
-    public DateTime GetCalendarDay(DateTime calendarStart, int calendarWeek, int dayInWeek)
+    public DateTime GetCalendarDay(DateTime calendarStart, int day)
     {
-        long ticks = calendarStart.Ticks + calendarWeek * _ticksInWeek + dayInWeek * _ticksInDay;
+        long ticks = calendarStart.Ticks + day * _ticksInDay;
 
         if (!_dateByTicks.TryGetValue(ticks, out DateTime date))
         {
