e48821c37109f3ae9003160cacdca06cf26b995c
Urban <dzindzinovic@hotmail.com>
2024-02-28 15:14:52 +0100

    added IStringLocalizer<Loc> Loc

--- Ididit/Services/CalendarService.cs
+++ Ididit/Services/CalendarService.cs
@@ -2,15 +2,13 @@
 
 public class CalendarService
 {
-    private readonly string[] _days = { "Su", "Mo", "Tu", "We", "Th", "Fr", "Sa" };
-
     private readonly Dictionary<long, DateTime> _dateByTicks = new();
 
     private const long _ticksInDay = 864_000_000_000;
 
-    public string GetDayOfWeek(DayOfWeek firstDayOfWeek, int dayIndex)
+    public int GetDayOfWeek(DayOfWeek firstDayOfWeek, int dayIndex)
     {
-        return _days[(dayIndex + (int)firstDayOfWeek) % 7];
+        return (dayIndex + (int)firstDayOfWeek) % 7;
     }
 
     public DateTime GetCalendarDay(DateTime calendarStart, int day)
