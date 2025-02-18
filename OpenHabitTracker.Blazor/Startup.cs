3de6171ba8f9c303fad0d518ef73066910a223f3
Urban <dzindzinovic@hotmail.com>
2025-02-18 12:28:30 +0100

    added STGTour.GTour

--- OpenHabitTracker.Blazor/Startup.cs
+++ OpenHabitTracker.Blazor/Startup.cs
@@ -1,3 +1,4 @@
+using GTour;
 using Microsoft.Extensions.DependencyInjection;
 using OpenHabitTracker.App;
 
@@ -10,6 +11,25 @@ public static class Startup
         services.AddScoped<JsInterop>();
         services.AddScoped<IRuntimeClientData, RuntimeClientData>();
 
+        services.UseGTour();
+        GTourService.Theme = new GTour.Themes.Bootstrap();
+
         return services;
     }
+}
+
+public class Bootstrap : GTour.Abstractions.ITheme
+{
+    public string GTourOverlay { get; set; } = "";
+    public string GTourWrapper { get; set; } = "modal ";
+    public string GTourArrow { get; set; } = "";
+    public string GTourStepWrapper { get; set; } = "modal-content ";
+    public string GTourStepHeaderWrapper { get; set; } = "modal-header ";
+    public string GTourStepContentWrapper { get; set; } = "modal-body ";
+    public string GTourStepFooterWrapper { get; set; } = "modal-footer ";
+    public string GTourStepHeaderTitle { get; set; } = "modal-title ";
+    public string GTourStepCancelButton { get; set; } = "btn btn-warning ";
+    public string GTourStepPreviousButton { get; set; } = "btn btn-secondary ";
+    public string GTourStepNextButton { get; set; } = "btn btn-primary ";
+    public string GTourStepCompleteButton { get; set; } = "btn btn-success ";
 }
