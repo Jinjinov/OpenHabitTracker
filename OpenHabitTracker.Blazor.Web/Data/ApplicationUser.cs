dd06b49747daa4d04cf4fe64563a7249775fb20c
Urban <dzindzinovic@hotmail.com>
2025-02-18 09:04:44 +0100

    fix ApplicationUser

--- OpenHabitTracker.Blazor.Web/Data/ApplicationUser.cs
+++ OpenHabitTracker.Blazor.Web/Data/ApplicationUser.cs
@@ -5,4 +5,21 @@ namespace OpenHabitTracker.Blazor.Web.Data;
 
 public class ApplicationUser : IdentityUser<long>, IUserEntity
 {
+    string IUserEntity.UserName
+    {
+        get => base.UserName!;
+        set => base.UserName = value;
+    }
+
+    string IUserEntity.Email
+    {
+        get => base.Email!;
+        set => base.Email = value;
+    }
+
+    string IUserEntity.PasswordHash
+    {
+        get => base.PasswordHash!;
+        set => base.PasswordHash = value;
+    }
 }
