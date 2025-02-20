2cf474e7e468de5a8f695fd8150da1f2280d706a
Urban <dzindzinovic@hotmail.com>
2025-02-20 08:46:24 +0100

    cleanup

--- OpenHabitTracker/Data/Models/HabitModel.cs
+++ OpenHabitTracker/Data/Models/HabitModel.cs
@@ -147,31 +147,4 @@ public class HabitModel : ItemsModel
             _ => throw new ArgumentOutOfRangeException(nameof(RepeatPeriod))
         };
     }
-
-    public bool? IsOverdue() // TODO:: add a field, call the method only when TimesDone changes
-    {
-        if (LastTimeDoneAt is null)
-            return null;
-
-        // TODO:: use RepeatCount
-
-        // do NOT use LastTimeDoneAt
-
-        // only use TimesDone
-
-        // sort it, because TimesDone can have elements in the past added later
-
-        // iterate the list from the end, break if you find RepeatCount num of dates, or if you reach a date that is outside elapsedTime
-
-        DateTime nextDueDate = RepeatPeriod switch
-        {
-            Period.Day => LastTimeDoneAt.Value.AddDays(RepeatInterval),
-            Period.Week => LastTimeDoneAt.Value.AddDays(7 * RepeatInterval),
-            Period.Month => LastTimeDoneAt.Value.AddMonths(RepeatInterval),
-            Period.Year => LastTimeDoneAt.Value.AddYears(RepeatInterval),
-            _ => throw new ArgumentOutOfRangeException(nameof(RepeatPeriod))
-        };
-
-        return nextDueDate < DateTime.Now;
-    }
 }
