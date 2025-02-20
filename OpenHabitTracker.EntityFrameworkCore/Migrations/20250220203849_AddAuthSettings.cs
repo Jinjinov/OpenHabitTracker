75ebd591f193e09bb63d4f0bf3d14acb9d83aee0
Urban <dzindzinovic@hotmail.com>
2025-02-20 21:57:31 +0100

    added migration AddAuthSettings

---
+++ OpenHabitTracker.EntityFrameworkCore/Migrations/20250220203849_AddAuthSettings.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OpenHabitTracker.EntityFrameworkCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAuthSettings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BaseUrl",
+                table: "Settings",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "Settings",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "RememberMe",
+                table: "Settings",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "ShowHelp",
+                table: "Settings",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "UncheckAllItemsOnHabitDone",
+                table: "Settings",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BaseUrl",
+                table: "Settings");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "Settings");
+
+            migrationBuilder.DropColumn(
+                name: "RememberMe",
+                table: "Settings");
+
+            migrationBuilder.DropColumn(
+                name: "ShowHelp",
+                table: "Settings");
+
+            migrationBuilder.DropColumn(
+                name: "UncheckAllItemsOnHabitDone",
+                table: "Settings");
+        }
+    }
+}
