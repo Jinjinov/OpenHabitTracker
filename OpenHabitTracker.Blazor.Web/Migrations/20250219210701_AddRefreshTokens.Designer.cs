ce8ae938efb997c9364636d2ef08d5ce90d25417
Urban <dzindzinovic@hotmail.com>
2025-02-20 01:11:39 +0100

    added migration AddRefreshTokens

--- OpenHabitTracker.Blazor.Web/Migrations/20250219210701_AddRefreshTokens.Designer.cs
+++ OpenHabitTracker.Blazor.Web/Migrations/20250219210701_AddRefreshTokens.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OpenHabitTracker.Blazor.Web.Data;
 
@@ -10,9 +11,11 @@ using OpenHabitTracker.Blazor.Web.Data;
 namespace OpenHabitTracker.Blazor.Web.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250219210701_AddRefreshTokens")]
+    partial class AddRefreshTokens
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.1");
---
+++ OpenHabitTracker.Blazor.Web/Migrations/20250219210701_AddRefreshTokens.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OpenHabitTracker.Blazor.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRefreshTokens : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BaseUrl",
+                table: "Settings",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "Settings",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "RememberMe",
+                table: "Settings",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "ShowHelp",
+                table: "Settings",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "UncheckAllItemsOnHabitDone",
+                table: "Settings",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "RefreshTokens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Username = table.Column<string>(type: "TEXT", nullable: false),
+                    Token = table.Column<string>(type: "TEXT", nullable: false),
+                    ExpiryDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RefreshTokens", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RefreshTokens");
+
+            migrationBuilder.DropColumn(
+                name: "BaseUrl",
+                table: "Settings");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "Settings");
+
+            migrationBuilder.DropColumn(
+                name: "RememberMe",
+                table: "Settings");
+
+            migrationBuilder.DropColumn(
+                name: "ShowHelp",
+                table: "Settings");
+
+            migrationBuilder.DropColumn(
+                name: "UncheckAllItemsOnHabitDone",
+                table: "Settings");
+        }
+    }
+}
