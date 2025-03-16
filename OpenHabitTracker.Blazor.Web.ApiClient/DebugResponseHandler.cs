d19b3b548166a35fed2ce5aaf7456e7d12d728a7
Urban <dzindzinovic@hotmail.com>
2025-03-16 11:46:57 +0100

    updated DebugResponseHandler

--- OpenHabitTracker.Blazor.Web.ApiClient/DebugResponseHandler.cs
+++ OpenHabitTracker.Blazor.Web.ApiClient/DebugResponseHandler.cs
@@ -1,15 +1,27 @@
+using System.Diagnostics;
+
 namespace OpenHabitTracker.Blazor.Web.ApiClient;
 
 public class DebugResponseHandler : DelegatingHandler
 {
     protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
+        Debug.WriteLine($"Request: {request.Method} {request.RequestUri}");
+
+        if (request.Content != null)
+        {
+            string rawRequest = await request.Content.ReadAsStringAsync().ConfigureAwait(false);
+            Debug.WriteLine("Request Content: " + rawRequest);
+        }
+
         var response = await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
+
+        Debug.WriteLine("Response StatusCode: " + response.StatusCode);
 
         if (response.Content != null)
         {
             string rawResponse = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-            System.Diagnostics.Debug.WriteLine(rawResponse);
+            Debug.WriteLine("Response Content: " + rawResponse);
         }
 
         return response;
