298649a4f42eb892b5922545294a80c9303211cd
Urban <dzindzinovic@yahoo.com>
2026-02-10 10:09:23 +0100

    added ShowHabitStatistics

---
+++ OpenHabitTracker.EntityFrameworkCore/Migrations/20260210090414_AddShowHabitStatistics.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OpenHabitTracker.EntityFrameworkCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddShowHabitStatistics : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ShowHabitStatistics",
+                table: "Settings",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShowHabitStatistics",
+                table: "Settings");
+        }
+    }
+}
