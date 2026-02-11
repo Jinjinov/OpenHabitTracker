545bf5b2dc01ef282314308a3dadabcf1c95c5da
Urban <dzindzinovic@yahoo.com>
2026-02-11 21:41:59 +0100

    added AddShowSettings

---
+++ OpenHabitTracker.Blazor.Web/Migrations/20260211204006_AddShowSettings.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OpenHabitTracker.Blazor.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddShowSettings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ShowCategory",
+                table: "Settings",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "ShowHabitStatistics",
+                table: "Settings",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "ShowPriorityDropdown",
+                table: "Settings",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShowCategory",
+                table: "Settings");
+
+            migrationBuilder.DropColumn(
+                name: "ShowHabitStatistics",
+                table: "Settings");
+
+            migrationBuilder.DropColumn(
+                name: "ShowPriorityDropdown",
+                table: "Settings");
+        }
+    }
+}
