c5a2d14516b2587f29f678e8fb815ec681b39116
Urban <dzindzinovic@yahoo.com>
2026-03-11 23:11:52 +0100

    added Arrow keys for Menu sidebar (ARIA menu pattern)

--- OpenHabitTracker.EndToEndTests/Accessibility/SidebarBehaviorTests.cs
+++ OpenHabitTracker.EndToEndTests/Accessibility/SidebarBehaviorTests.cs
@@ -62,4 +62,76 @@ public class SidebarBehaviorTests : BaseTest
 
         await Expect(Page.Locator("div.col-12.col-md-2.child-column")).Not.ToBeVisibleAsync();
     }
+
+    [Test]
+    public async Task Menu_ArrowDown_MovesFocusToNextItem()
+    {
+        await OpenMenuAsync();
+
+        await Page.Locator("div[role='menu'] button:has(i.bi-trash)").FocusAsync();
+        await Page.Keyboard.PressAsync("ArrowDown");
+
+        await Expect(Page.Locator("div[role='menu'] button:has(i.bi-search)")).ToBeFocusedAsync();
+    }
+
+    [Test]
+    public async Task Menu_ArrowUp_MovesFocusToPreviousItem()
+    {
+        await OpenMenuAsync();
+
+        await Page.Locator("div[role='menu'] button:has(i.bi-trash)").FocusAsync();
+        await Page.Keyboard.PressAsync("ArrowDown");
+        await Expect(Page.Locator("div[role='menu'] button:has(i.bi-search)")).ToBeFocusedAsync();
+        await Page.Keyboard.PressAsync("ArrowUp");
+
+        await Expect(Page.Locator("div[role='menu'] button:has(i.bi-trash)")).ToBeFocusedAsync();
+    }
+
+    [Test]
+    public async Task Menu_ArrowDown_FromLastItem_WrapsFocusToFirstItem()
+    {
+        await OpenMenuAsync();
+
+        await Page.Locator("div[role='menu'] button:has(i.bi-trash)").FocusAsync();
+        await Page.Keyboard.PressAsync("End");
+        await Expect(Page.Locator("div[role='menu'] button:has(i.bi-info-circle)")).ToBeFocusedAsync();
+        await Page.Keyboard.PressAsync("ArrowDown");
+
+        await Expect(Page.Locator("div[role='menu'] button:has(i.bi-trash)")).ToBeFocusedAsync();
+    }
+
+    [Test]
+    public async Task Menu_ArrowUp_FromFirstItem_WrapsFocusToLastItem()
+    {
+        await OpenMenuAsync();
+
+        await Page.Locator("div[role='menu'] button:has(i.bi-trash)").FocusAsync();
+        await Page.Keyboard.PressAsync("ArrowUp");
+
+        await Expect(Page.Locator("div[role='menu'] button:has(i.bi-info-circle)")).ToBeFocusedAsync();
+    }
+
+    [Test]
+    public async Task Menu_HomeKey_MovesFocusToFirstItem()
+    {
+        await OpenMenuAsync();
+
+        await Page.Locator("div[role='menu'] button:has(i.bi-trash)").FocusAsync();
+        await Page.Keyboard.PressAsync("ArrowDown");
+        await Page.Keyboard.PressAsync("ArrowDown");
+        await Page.Keyboard.PressAsync("Home");
+
+        await Expect(Page.Locator("div[role='menu'] button:has(i.bi-trash)")).ToBeFocusedAsync();
+    }
+
+    [Test]
+    public async Task Menu_EndKey_MovesFocusToLastItem()
+    {
+        await OpenMenuAsync();
+
+        await Page.Locator("div[role='menu'] button:has(i.bi-trash)").FocusAsync();
+        await Page.Keyboard.PressAsync("End");
+
+        await Expect(Page.Locator("div[role='menu'] button:has(i.bi-info-circle)")).ToBeFocusedAsync();
+    }
 }
--- OpenHabitTracker.UnitTests/Components/MenuComponentTests.cs
+++ OpenHabitTracker.UnitTests/Components/MenuComponentTests.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Localization;
 using NSubstitute;
+using OpenHabitTracker.Blazor;
 using OpenHabitTracker.Blazor.Pages;
 
 namespace OpenHabitTracker.UnitTests.Components;
@@ -12,6 +13,7 @@ namespace OpenHabitTracker.UnitTests.Components;
 public class MenuComponentTests
 {
     private BunitContext _ctx = null!;
+    private IJsInterop _jsInterop = null!;
 
     [SetUp]
     public void SetUp()
@@ -21,7 +23,10 @@ public class MenuComponentTests
         IStringLocalizer loc = Substitute.For<IStringLocalizer>();
         loc[Arg.Any<string>()].Returns(callInfo => new LocalizedString(callInfo.Arg<string>(), callInfo.Arg<string>()));
 
+        _jsInterop = Substitute.For<IJsInterop>();
+
         _ctx.Services.AddSingleton(loc);
+        _ctx.Services.AddScoped(_ => _jsInterop);
     }
 
     [TearDown]
@@ -64,5 +69,99 @@ public class MenuComponentTests
         await cut.Find("button:has(i.bi-gear)").ClickAsync(new MouseEventArgs());
 
         Assert.That(invokedWith, Is.EqualTo(typeof(Settings)));
+    }
+
+    [Test]
+    public void Renders_FirstButtonWithTabindexZero()
+    {
+        IRenderedComponent<Menu> cut = _ctx.Render<Menu>();
+
+        IReadOnlyList<IElement> buttons = cut.FindAll("div.list-group > button");
+
+        Assert.That(buttons[0].GetAttribute("tabindex"), Is.EqualTo("0"));
+        Assert.That(buttons[1].GetAttribute("tabindex"), Is.EqualTo("-1"));
+    }
+
+    [Test]
+    public async Task ArrowDown_MovesTabindexToNextButton()
+    {
+        IRenderedComponent<Menu> cut = _ctx.Render<Menu>();
+
+        await cut.Find("div[role='menu']").KeyDownAsync(new KeyboardEventArgs { Key = "ArrowDown" });
+
+        IReadOnlyList<IElement> buttons = cut.FindAll("div.list-group > button");
+        Assert.That(buttons[0].GetAttribute("tabindex"), Is.EqualTo("-1"));
+        Assert.That(buttons[1].GetAttribute("tabindex"), Is.EqualTo("0"));
+    }
+
+    [Test]
+    public async Task ArrowDown_FromLastButton_WrapsToFirstButton()
+    {
+        IRenderedComponent<Menu> cut = _ctx.Render<Menu>();
+
+        for (int i = 0; i < 6; i++)
+            await cut.Find("div[role='menu']").KeyDownAsync(new KeyboardEventArgs { Key = "ArrowDown" });
+
+        IReadOnlyList<IElement> buttons = cut.FindAll("div.list-group > button");
+        Assert.That(buttons[0].GetAttribute("tabindex"), Is.EqualTo("0"));
+    }
+
+    [Test]
+    public async Task ArrowUp_FromFirstButton_WrapsToLastButton()
+    {
+        IRenderedComponent<Menu> cut = _ctx.Render<Menu>();
+
+        await cut.Find("div[role='menu']").KeyDownAsync(new KeyboardEventArgs { Key = "ArrowUp" });
+
+        IReadOnlyList<IElement> buttons = cut.FindAll("div.list-group > button");
+        Assert.That(buttons[5].GetAttribute("tabindex"), Is.EqualTo("0"));
+        Assert.That(buttons[0].GetAttribute("tabindex"), Is.EqualTo("-1"));
+    }
+
+    [Test]
+    public async Task Home_MovesTabindexToFirstButton()
+    {
+        IRenderedComponent<Menu> cut = _ctx.Render<Menu>();
+
+        await cut.Find("div[role='menu']").KeyDownAsync(new KeyboardEventArgs { Key = "ArrowDown" });
+        await cut.Find("div[role='menu']").KeyDownAsync(new KeyboardEventArgs { Key = "ArrowDown" });
+        await cut.Find("div[role='menu']").KeyDownAsync(new KeyboardEventArgs { Key = "Home" });
+
+        IReadOnlyList<IElement> buttons = cut.FindAll("div.list-group > button");
+        Assert.That(buttons[0].GetAttribute("tabindex"), Is.EqualTo("0"));
+    }
+
+    [Test]
+    public async Task End_MovesTabindexToLastButton()
+    {
+        IRenderedComponent<Menu> cut = _ctx.Render<Menu>();
+
+        await cut.Find("div[role='menu']").KeyDownAsync(new KeyboardEventArgs { Key = "End" });
+
+        IReadOnlyList<IElement> buttons = cut.FindAll("div.list-group > button");
+        Assert.That(buttons[5].GetAttribute("tabindex"), Is.EqualTo("0"));
+        Assert.That(buttons[0].GetAttribute("tabindex"), Is.EqualTo("-1"));
+    }
+
+    [Test]
+    public async Task ClickSecondButton_UpdatesTabindexToSecondButton()
+    {
+        IRenderedComponent<Menu> cut = _ctx.Render<Menu>();
+
+        await cut.Find("button:has(i.bi-search)").ClickAsync(new MouseEventArgs());
+
+        IReadOnlyList<IElement> buttons = cut.FindAll("div.list-group > button");
+        Assert.That(buttons[1].GetAttribute("tabindex"), Is.EqualTo("0"));
+        Assert.That(buttons[0].GetAttribute("tabindex"), Is.EqualTo("-1"));
+    }
+
+    [Test]
+    public async Task ArrowDown_CallsFocusElementOnJsInterop()
+    {
+        IRenderedComponent<Menu> cut = _ctx.Render<Menu>();
+
+        await cut.Find("div[role='menu']").KeyDownAsync(new KeyboardEventArgs { Key = "ArrowDown" });
+
+        await _jsInterop.Received(1).FocusElement(Arg.Any<Microsoft.AspNetCore.Components.ElementReference>());
     }
 }
