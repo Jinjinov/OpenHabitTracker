56e4fc95239c3b8265062a89d08d53c22c8cac33
Urban <dzindzinovic@yahoo.com>
2026-03-17 13:26:19 +0100

    cleanup

--- OpenHabitTracker/Data/Models/ItemsModel.cs
+++ OpenHabitTracker/Data/Models/ItemsModel.cs
@@ -1,6 +1,26 @@
-﻿namespace OpenHabitTracker.Data.Models;
+namespace OpenHabitTracker.Data.Models;
 
 public class ItemsModel : ContentModel
 {
     public List<ItemModel>? Items { get; set; }
+
+    public TimeOnly? Duration { get; set; }
+
+    internal TimeOnly DurationProxy
+    {
+        get => Duration ?? TimeOnly.MinValue;
+        set => Duration = value == TimeOnly.MinValue ? null : value;
+    }
+
+    internal int DurationHour
+    {
+        get => DurationProxy.Hour;
+        set => DurationProxy = new TimeOnly(value, DurationProxy.Minute);
+    }
+
+    internal int DurationMinute
+    {
+        get => DurationProxy.Minute;
+        set => DurationProxy = new TimeOnly(DurationProxy.Hour, value);
+    }
 }
