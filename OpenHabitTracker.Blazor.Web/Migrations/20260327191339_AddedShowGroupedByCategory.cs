328553a07c1126f42087378a4779483fafea41e9
Urban <dzindzinovic@yahoo.com>
2026-03-27 20:20:06 +0100

    added migration AddedShowGroupedByCategory

---
+++ OpenHabitTracker.Blazor.Web/Migrations/20260327191339_AddedShowGroupedByCategory.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OpenHabitTracker.Blazor.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedShowGroupedByCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ShowGroupedByCategory",
+                table: "Settings",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "ShowLastTimeDone",
+                table: "Settings",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CompletionRule",
+                table: "Categories",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCollapsed",
+                table: "Categories",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShowGroupedByCategory",
+                table: "Settings");
+
+            migrationBuilder.DropColumn(
+                name: "ShowLastTimeDone",
+                table: "Settings");
+
+            migrationBuilder.DropColumn(
+                name: "CompletionRule",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "IsCollapsed",
+                table: "Categories");
+        }
+    }
+}
