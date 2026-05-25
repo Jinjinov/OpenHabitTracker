46eb6a9b4b701d9b3415479ade1f77b339598159
Urban <dzindzinovic@yahoo.com>
2026-05-25 15:19:37 +0200

    added migration AddDisplayMetricAndQuantity

---
+++ OpenHabitTracker.EntityFrameworkCore/Migrations/20260525131548_AddDisplayMetricAndQuantity.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OpenHabitTracker.EntityFrameworkCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDisplayMetricAndQuantity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "Quantity",
+                table: "Times",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DisplayMetric",
+                table: "Contents",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "TargetQuantity",
+                table: "Contents",
+                type: "INTEGER",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "Times");
+
+            migrationBuilder.DropColumn(
+                name: "DisplayMetric",
+                table: "Contents");
+
+            migrationBuilder.DropColumn(
+                name: "TargetQuantity",
+                table: "Contents");
+        }
+    }
+}
