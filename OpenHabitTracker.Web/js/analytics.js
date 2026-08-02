31b3a6a42e4065004baa16eb557c5ce8fae2fd51
Urban <dzindzinovic@yahoo.com>
2026-08-02 17:31:58 +0200

    added analytics.js

---
+++ OpenHabitTracker.Web/js/analytics.js
@@ -0,0 +1,75 @@
+function onlineAppUsage() {
+  gtag('event', 'oht_online_app_usage', {'transport': 'beacon'});
+}
+function microsoftStoreVisit() {
+  gtag('event', 'oht_microsoft_store_visit', {'transport': 'beacon'});
+}
+function flathubStoreVisit() {
+  gtag('event', 'oht_flathub_store_visit', {'transport': 'beacon'});
+}
+function snapStoreVisit() {
+  gtag('event', 'oht_snap_store_visit', {'transport': 'beacon'});
+}
+function googleStoreVisit() {
+  gtag('event', 'oht_google_store_visit', {'transport': 'beacon'});
+}
+function iosStoreVisit() {
+  gtag('event', 'oht_ios_store_visit', {'transport': 'beacon'});
+}
+function macosStoreVisit() {
+  gtag('event', 'oht_macos_store_visit', {'transport': 'beacon'});
+}
+function apkpureStoreVisit() {
+  gtag('event', 'oht_apkpure_store_visit', {'transport': 'beacon'});
+}
+function gitHubReleasesVisit() {
+  gtag('event', 'oht_github_releases_visit', {'transport': 'beacon'});
+}
+function obtainiumVisit() {
+  gtag('event', 'oht_obtainium_visit', {'transport': 'beacon'});
+}
+function windowsAppDownload() {
+  gtag('event', 'oht_windows_app_download', {'transport': 'beacon'});
+}
+function linuxAppDownload() {
+  gtag('event', 'oht_linux_app_download', {'transport': 'beacon'});
+}
+function macosAppDownload() {
+  gtag('event', 'oht_macos_app_download', {'transport': 'beacon'});
+}
+function androidAppDownload() {
+  gtag('event', 'oht_android_app_download', {'transport': 'beacon'});
+}
+function dockerHubVisit() {
+  gtag('event', 'oht_docker_hub_visit', {'transport': 'beacon'});
+}
+function gitHubContainerRegistryVisit() {
+  gtag('event', 'oht_github_container_registry_visit', {'transport': 'beacon'});
+}
+function gitHubVisit() {
+  gtag('event', 'oht_github_visit', {'transport': 'beacon'});
+}
+function gitHubDiscussionsVisit() {
+  gtag('event', 'oht_github_discussions_visit', {'transport': 'beacon'});
+}
+function redditVisit() {
+  gtag('event', 'oht_reddit_visit', {'transport': 'beacon'});
+}
+function loopComparisonVisit() {
+  gtag('event', 'oht_loop_comparison_visit', {'transport': 'beacon'});
+}
+function googleKeepComparisonVisit() {
+  gtag('event', 'oht_google_keep_comparison_visit', {'transport': 'beacon'});
+}
+function habiticaComparisonVisit() {
+  gtag('event', 'oht_habitica_comparison_visit', {'transport': 'beacon'});
+}
+function streaksComparisonVisit() {
+  gtag('event', 'oht_streaks_comparison_visit', {'transport': 'beacon'});
+}
+function everydayComparisonVisit() {
+  gtag('event', 'oht_everyday_comparison_visit', {'transport': 'beacon'});
+}
+function fullComparisonVisit() {
+  gtag('event', 'oht_full_comparison_visit', {'transport': 'beacon'});
+}
